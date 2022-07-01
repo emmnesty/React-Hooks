0c0a771544dc0d4a9eaa9cf024e6fcd7cf1c0df6
emmnesty <elyjhonatan@hotmail.com>
2022-07-01 10:45:10 -0300

    Corrigindo bug no useEffect

--- src/Components/useEffect.jsx
+++ src/Components/useEffect.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 function calcFatorial(num) {
   const n = parseInt(num);
+
   if (n < 0) return -1;
   if (n === 0) return 1;
   return calcFatorial(n - 1) * n;
@@ -9,11 +10,12 @@ function calcFatorial(num) {
 
 const Fatorial = (props) => {
   const [number, setNumber] = useState(1);
-  const [fatorial, setFatorial] = useState(1);
+  const [fatorial, setFatorial] = useState('');
 
   useEffect(
     function () {
-      setFatorial(calcFatorial(number));
+      if (number === null || number == '') setFatorial('Digitar um número');
+      else setFatorial(calcFatorial(number));
     },
     [number]
   );
@@ -29,12 +31,9 @@ const Fatorial = (props) => {
 
   const [status, setStatus] = useState('Ímpar');
 
-  useEffect(
-    function () {
-      setStatus(number % 2 === 0 ? 'Par' : 'Ímpar');
-    },
-    []
-  );
+  useEffect(function () {
+    setStatus(number % 2 === 0 ? 'Par' : 'Ímpar');
+  }, []);
 
   return (
     <div>
